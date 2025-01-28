bdf2f29ca1b78b66f94da782a955d24ffa5c1b57
Paul HILAIRE <paul.hilaire@quandela.com>
2025-01-28 22:53:06 +0100

    add dynamic box for research interests

--- js/script.js
+++ js/script.js
@@ -7,3 +7,34 @@ document.addEventListener('DOMContentLoaded', function() {
         startNumber += list.querySelectorAll('li').length;
     });
 });
+
+// document.querySelector('.read-more-btn').addEventListener('click', function () {
+//     const fullText = document.querySelector('.full-text');
+//     const shortText = document.querySelector('.short-text');
+//     if (fullText.style.display === 'none') {
+//         fullText.style.display = 'block';
+//         shortText.style.display = 'none';
+//         this.textContent = 'Read less'; // Change button text
+//     } else {
+//         fullText.style.display = 'none';
+//         shortText.style.display = 'block';
+//         this.textContent = 'Read more'; // Reset button text
+//     }
+// });
+
+document.querySelectorAll('.read-more-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        const fullText = this.closest('.right').querySelector('.full-text');
+        const shortText = this.closest('.right').querySelector('.short-text');
+        
+        if (fullText.style.display === 'none') {
+            fullText.style.display = 'block';
+            shortText.style.display = 'none';
+            this.textContent = 'Read less'; // Change button text
+        } else {
+            fullText.style.display = 'none';
+            shortText.style.display = 'block';
+            this.textContent = 'Read more'; // Reset button text
+        }
+    });
+});
